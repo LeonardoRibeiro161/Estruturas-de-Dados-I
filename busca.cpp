91ba47b66bf52f1a2533e60477d4ee27a256095c
Leonardo Ribeiro <48728011+LeonardoRibeiro161@users.noreply.github.com>
2019-06-09 15:05:21 -0300

    Add files via upload

---
+++ busca.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+int busca_binaria(int tamanho, int *v , int busca);
+int ordenar(int *vetor, int tamanho)
+{
+	
+	int aux, i, j, busca, num;
+	for( i = 0; i < tamanho ; i++)
+	{
+		for ( j = 0; j < tamanho; j++)
+		{
+			if (vetor[i] < vetor[j])
+			{
+				aux = vetor[i];
+				vetor[i] = vetor[j];
+				vetor[j] = aux;
+			}
+		}
+	}
+	cout<<"\n Vetor ordenado "<<endl;
+	
+	for(i = 0; i < tamanho; i++)
+	{
+		cout<<vetor[i] << " - ";
+	}
+	cout<<"\n";
+	cout<<"Informe o numero que deseja buscar:";
+	cin>>busca;
+	
+	cout <<"O numero "<<busca<<" esta na posicao: "<<busca_binaria(tamanho, vetor, busca)<<endl;
+	
+}
+
+int busca_binaria(int tamanho, int *v , int busca)
+{
+	int ini = 0;
+	int fim = tamanho -1;
+	int meio;
+	
+	while(ini <= fim)
+	{
+		meio = (ini + fim )/2;
+		if(busca < v[meio])
+		{
+			fim = meio -1;
+			
+		}
+		else if(busca > v[meio])
+		{
+			ini = meio +1;
+		}
+		else
+		{
+			return meio;
+		}
+	}return cout<<"O numero nao esta no vetor";
+	
+}
+
+int main()
+{
+	
+	int vetor[6], vetor_ordenado[6];
+	int i, num, tamanho;
+	
+	for(i = 0; i < 6; i++)
+	{
+		cout<<"Informe o "<<i<< " numero: ";
+		cin>>vetor[i];		
+	}
+	tamanho = sizeof(vetor) / sizeof(int);
+	cout<<"\n Tamanho do vetor: "<<tamanho;
+	ordenar(vetor, tamanho);
+	
+		
+}
